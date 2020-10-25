40b36ffc8060f2378a66c08e78be2634c5289fc4
dahee8kim <dahee8kim@gmail.com>
2020-10-25 22:17:33 +0900

    Add: NS(Network Service) Controller

--- src/main/java/com/dahee8kim/monitoring/controller/NetworkServiceController.java
+++ src/main/java/com/dahee8kim/monitoring/controller/NetworkServiceController.java
@@ -1,12 +1,20 @@
 package com.dahee8kim.monitoring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class NetworkServiceController {
     @GetMapping("/ns")
     public String NetworkService() {
         return "ns/index";
+    }
+
+    @GetMapping("/ns/detail")
+    public String NetworkServiceDetail(@RequestParam("id") String id, Model model) {
+        model.addAttribute("id", id);
+        return "ns/detail";
     }
 }
