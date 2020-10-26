d89a86c5e9f06e15e42f39aa49b9c029eb83e39e
dahee8kim <dahee8kim@gmail.com>
2020-10-26 12:03:31 +0900

    Modified: add controller module from NSController.java

--- src/main/java/com/dahee8kim/monitoring/controller/HomeController.java
+++ src/main/java/com/dahee8kim/monitoring/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.dahee8kim.monitoring.controller;
 
 import com.dahee8kim.monitoring.domain.osm.Token;
+import com.dahee8kim.monitoring.restAPI.osm.NSController;
 import com.dahee8kim.monitoring.restAPI.osm.TokenController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,9 +12,12 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         TokenController tokenController = new TokenController();
+        NSController nsController = new NSController();
 
         // 토큰 정보
         Token token = tokenController.getToken();
+        System.out.println(nsController.getNS(token.getToken()));
+
         return "index";
     }
 }
