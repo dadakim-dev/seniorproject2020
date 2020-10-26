08d1bbfd13059f0bed01c9449b31b2388db1c9bf
dahee8kim <dahee8kim@gmail.com>
2020-10-26 11:18:29 +0900

    Create: Token domain (included getter, setter)

---
+++ src/main/java/com/dahee8kim/monitoring/domain/osm/Token.java
@@ -0,0 +1,40 @@
+package com.dahee8kim.monitoring.domain.osm;
+
+public class Token {
+    private String token;
+    private String userId;
+    private String userName;
+    private String projectId;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+}
