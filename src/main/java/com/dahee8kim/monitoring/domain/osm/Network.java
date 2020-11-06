2bdbeef7db3b143df0300fb0902d0db2122581d6
dahee8kim <dahee8kim@gmail.com>
2020-11-06 23:57:09 +0900

    Create: network domain

---
+++ src/main/java/com/dahee8kim/monitoring/domain/osm/Network.java
@@ -0,0 +1,13 @@
+package com.dahee8kim.monitoring.domain.osm;
+
+public class Network {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
