0e32335d299495781f535bb64b526aad197bc16a
Kim Dahee <dahee8kim@gmail.com>
2020-11-07 20:56:39 +0900

    Update: home resource usage status

--- src/main/java/com/dahee8kim/monitoring/controller/HomeController.java
+++ src/main/java/com/dahee8kim/monitoring/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.dahee8kim.monitoring.controller;
 import com.dahee8kim.monitoring.restAPI.openstack.OpenStackTokenController;
+import com.dahee8kim.monitoring.restAPI.openstack.ResourceController;
 import com.dahee8kim.monitoring.restAPI.osm.NSController;
 import com.dahee8kim.monitoring.restAPI.osm.OSMTokenController;
 import org.springframework.stereotype.Controller;
@@ -10,13 +11,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
+        // get osm token
         OSMTokenController osmTokenController = new OSMTokenController();
+        String osmToken = osmTokenController.getToken();
+
+        // get openstack token
         OpenStackTokenController openStackTokenController = new OpenStackTokenController();
+        String openStackToken = openStackTokenController.getToken();
 
+        // get ns list and add to model
         NSController nsController = new NSController();
-        nsController.setToken(osmTokenController.getToken());
-
+        nsController.setToken(osmToken);
         model.addAttribute("NSs", nsController.getNSs());
+
+        // get resource state and add to model
+        ResourceController resourceController = new ResourceController();
+        resourceController.setToken(openStackToken);
+        model.addAttribute("resource", resourceController.getResource());
+
         return "index";
     }
 }
